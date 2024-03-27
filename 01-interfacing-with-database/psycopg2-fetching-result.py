dcae9ea2fa0bbcd31e8ea8eb055fa77debded046
itsnot-aduck <hoangduc1662002@gmail.com>
2024-03-27 14:08:34 +0700

    add: fetch-result.py

---
+++ 01-interfacing-with-database/psycopg2-fetching-result.py
@@ -0,0 +1,45 @@
+import psycopg2 
+
+connection = psycopg2.connect(dbname = "example", user = "postgres", host = "localhost", password = "1234")
+
+cursor = connection.cursor()
+
+# DROP if existed 
+cursor.execute('DROP TABLE IF EXISTS table3;')
+
+cursor.execute('''
+               CREATE TABLE table3(
+                    id INTEGER PRIMARY KEY,
+                    completed BOOLEAN NOT NULL DEFAULT False
+               );
+               ''')
+
+# Pass by string
+cursor.execute('INSERT INTO table3 (id, completed) VALUES (%s, %s);', (1, True))
+
+# Pass by tuple
+cursor.execute('INSERT INTO table3 (id, completed) ' + 'VALUES (%(id)s, %(completed)s);', {
+    'id': 2,
+    'completed': False
+})
+
+data = {
+    'id': 3,
+    'completed': False
+}
+
+SQL = 'INSERT INTO table3 (id, completed) ' + 'VALUES (%(id)s, %(completed)s);'
+# Pass by object 
+cursor.execute(SQL, data)
+
+cursor.execute('SELECT * from table3;')
+# Fetch all results
+result = cursor.fetchall()
+print(result)
+
+# Commit the work to database
+connection.commit()
+
+# Close the connection
+cursor.close()
+connection.close()
