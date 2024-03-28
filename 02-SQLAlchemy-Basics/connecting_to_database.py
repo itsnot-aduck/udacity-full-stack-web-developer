2f5c148596c6d05ff76ae34cf81875fdfc2df82c
itsnot-aduck <hoangduc1662002@gmail.com>
2024-03-28 15:11:48 +0700

    add: SQLAlchemy basics

---
+++ 02-SQLAlchemy-Basics/connecting_to_database.py
@@ -0,0 +1,27 @@
+# Run command: python3 -m flask --app .\01-connecting-to-database.py run
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1234@localhost:5432/example'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Intialize the instance of SQLAlchemy
+db = SQLAlchemy(app)
+app.app_context().push()
+
+class Person(db.Model):
+    __tablename__ = 'persons'
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(), nullable = False)
+
+    def __repr__(self):
+        return f'<Person ID: {self.id}, name = {self.name}>'
+# Detect modules and creates tables for them (if they don't exist)
+db.create_all()
+
+@app.route('/')
+def index():
+    person = Person.query.first()
+    return 'Hello ' + person.name
